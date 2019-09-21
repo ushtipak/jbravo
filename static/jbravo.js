5a24989182aaa47d90800aa1e44b74866021783b
Milos Knezevic <ushtipak@gmail.com>
2019-09-21 19:32:08 +0200

    Add onload date retrieval :)

--- static/jbravo.js
+++ static/jbravo.js
@@ -11,3 +11,13 @@ function update(row) {
     doneObj.value = "";
     letterObj.textContent = String.fromCharCode(total);
 }
+
+function getDate() {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+
+    today = dd + '-' + mm + '-' + yyyy;
+    console.log(today);
+}
