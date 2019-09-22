37b2ea25bdbb653ad7f98656f55ef295b4321df2
Milos Knezevic <ushtipak@gmail.com>
2019-09-22 13:28:46 +0200

    Update enableToday to highlight .. well today :)

--- static/jbravo.js
+++ static/jbravo.js
@@ -12,17 +12,22 @@ function update(row) {
     letterObj.textContent = String.fromCharCode(total);
 }
 
-function getDate() {
+function enableToday() {
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
-
     today = dd + '-' + mm + '-' + yyyy;
-    console.log(today);
+
+    let containers = document.getElementsByClassName("flex-container");
+    for (let i = 0; i < containers.length; i++) {
+        containers[i].classList.remove("selected");
+    }
 
     let doneObj = document.getElementById(today + "-done");
     let updateObj = document.getElementById(today + "-update");
+    let containerObj = document.getElementById(today);
     doneObj.disabled = false;
     updateObj.disabled = false;
+    containerObj.classList.add("selected");
 }
