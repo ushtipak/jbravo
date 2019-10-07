a30372ae35196535400c34488e9cf378c8c8cac2
Milos Knezevic <ushtipak@gmail.com>
2019-10-07 17:25:55 +0200

    Fix enable / disable of inputs for today / yesterday :)

--- static/jbravo.js
+++ static/jbravo.js
@@ -12,23 +12,37 @@ function update(row) {
     letterObj.textContent = String.fromCharCode(total);
 }
 
+function getDDMMYYYY(date) {
+    let dd = String(date.getDate()).padStart(2, "0");
+    let mm = String(date.getMonth() + 1).padStart(2, "0");
+    let yyyy = date.getFullYear();
+    return dd + "-" + mm + "-" + yyyy;
+}
+
 function enableToday() {
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0');
-    let yyyy = today.getFullYear();
-    today = dd + '-' + mm + '-' + yyyy;
-
     let containers = document.getElementsByClassName("flex-container");
-    for (let i = 0; i < containers.length; i++) {
-        containers[i].classList.remove("selected");
-    }
+    [].forEach.call(containers, function (container) { container.classList.remove("selected"); });
 
-    let doneObj = document.getElementById(today + "-done");
-    let updateObj = document.getElementById(today + "-update");
-    let containerObj = document.getElementById(today);
-    doneObj.disabled = false;
-    updateObj.disabled = false;
+    let today = new Date();
+    let todayStr = getDDMMYYYY(today);
+
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    let yesterdayStr = getDDMMYYYY(yesterday);
+
+    let yesterdayDoneObj = document.getElementById(yesterdayStr + "-done");
+    let yesterdayUpdateObj = document.getElementById(yesterdayStr + "-update");
+    if (yesterdayDoneObj != null) {
+        yesterdayDoneObj.disabled = true;
+        yesterdayDoneObj.value = "—";
+    }
+    if (yesterdayUpdateObj != null) { yesterdayUpdateObj.disabled = true; }
+
+    let todayDoneObj = document.getElementById(todayStr + "-done");
+    let todayUpdateObj = document.getElementById(todayStr + "-update");
+    let containerObj = document.getElementById(todayStr);
+    todayUpdateObj.disabled = false;
+    todayDoneObj.disabled = false;
     containerObj.classList.add("selected");
 }
 
